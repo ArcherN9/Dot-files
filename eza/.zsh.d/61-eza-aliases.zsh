94d4897bc485d77b88ae7f790df6eeb499701d42
ArcherN9 <Archern9@protonmail.com>
2026-02-12 22:34:59 +0100

    Replace conventional ls with eza

---
+++ eza/.zsh.d/61-eza-aliases.zsh
@@ -0,0 +1,6 @@
+# eza aliases - modern ls replacement
+alias ls='eza --icons'
+alias lsa='eza -la --icons'
+alias ll='eza -l --icons --git'
+alias la='eza -a --icons'
+alias lt='eza --tree --icons'
--- zsh-common/.zsh.d/60-aliases.zsh
+++ zsh-common/.zsh.d/60-aliases.zsh
@@ -2,6 +2,4 @@
 alias c="clear"
 alias ssh="TERM=xterm-256color $(which ssh)"
 alias stripJson=strip-json-comments
-alias lsa='ls -lsa --color=auto'
-alias ls='ls --color=auto'
 alias ..='cd ..'
